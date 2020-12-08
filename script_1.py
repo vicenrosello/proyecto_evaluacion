fdb79908e205f7ff87e072b4c8002c05b60d589b
David <david.rc.593@gmail.com>
2020-12-08 14:44:55 +0100

    nuevas modificaciones script_1

--- script_1.py
+++ script_1.py
@@ -2,7 +2,9 @@ import numpy as np
 
 lista = []
 for x in range(10):
-    if x < 5:
-        lista.append(x)
-        print(x)
+    # Modificaciones master
+    for y in lista:
+        if y % x == 0:
+            return y
 
+print("Modificaciones script_1 master")
